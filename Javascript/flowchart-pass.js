d11301dca70c05c9da864b5cd6e5f8449fb51869
Nicolai Amirzadeh <nicolai.amirzadeh@outlook.com>
2021-05-02 18:20:27 +0200

    Newest update

---
+++ Javascript/flowchart-pass.js
@@ -0,0 +1,14 @@
+function run() {
+    var password = "perception";
+    var x = prompt("Enter the password");
+    if (x.toLowerCase() == password) {
+        alert("Wait, how did you know the password?");
+        window.location = "flowchart.html";
+        }
+else {
+alert("Maybe play the game first?");
+alert("Understand?");
+alert("Are you sure?");
+ window.location = "index.html";
+}
+}
